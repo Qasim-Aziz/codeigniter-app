fd1e902d3889b64ec0a147da1dca551d4b18f77a
qasim683 <envoyfacilitation@outlook.com>
2021-02-19 02:35:37 +0500

    sql conf add

--- app/application/config/config.php
+++ app/application/config/config.php
@@ -1,4 +1,3 @@
-
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
@@ -24,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://ci-web';
+$config['base_url'] = 'http://0.0.0.0';
 
 /*
 |--------------------------------------------------------------------------
@@ -381,7 +380,7 @@ $config['encryption_key'] = '$-f_.CQH>~ZJV|QfJ*!16-8S#.e*ZUot<iGCL/WBUh\o';
 $config['sess_driver'] = 'files';
 $config['sess_cookie_name'] = 'ci_session';
 $config['sess_expiration'] = 0;
-$config['sess_save_path'] = NULL;
+$config['sess_save_path'] = sys_get_temp_dir();
 $config['sess_match_ip'] = FALSE;
 $config['sess_time_to_update'] = 300;
 $config['sess_regenerate_destroy'] = FALSE;
