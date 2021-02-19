1fdcfa8ac09d575021beb93d4b96af0139bfc9a0
qasim683 <envoyfacilitation@outlook.com>
2021-02-19 15:53:44 +0500

    testing with nip host

--- app/application/config/config.php
+++ app/application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://ci-deploy';
+$config['base_url'] = 'http://frontend.34.121.19.11.nip.io';
 
 /*
 |--------------------------------------------------------------------------
