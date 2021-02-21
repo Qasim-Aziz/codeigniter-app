38752f4692eb47ddfbc9d6a15dbb22f1f2398854
qasim683 <envoyfacilitation@outlook.com>
2021-02-21 15:42:28 +0500

    successfully testing

--- app/application/config/config.php
+++ app/application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/';
+$config['base_url'] = 'http://frontend.34.121.19.11.nip.io/';
 
 /*
 |--------------------------------------------------------------------------
@@ -402,7 +402,7 @@ $config['sess_regenerate_destroy'] = FALSE;
 |
 */
 $config['cookie_prefix']	= '';
-$config['cookie_domain']	= '';
+$config['cookie_domain']	= 'http://frontend.34.121.19.11.nip.io/';
 $config['cookie_path']		= '/';
 $config['cookie_secure']	= FALSE;
 $config['cookie_httponly'] 	= FALSE;
